d992dd2476ebdc1652af73ec5136483d79b248bc
Bidicte <malexandra13@yahoo.com>
2023-02-26 15:06:58 +0000

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int n;
+char ch;
+for (n = 0; n <= 9; n++)
+{
+putchar((n % 15) + '0');
+}
+
+for (ch = 'a'; ch <= 'e'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');   
+return (0);
+}

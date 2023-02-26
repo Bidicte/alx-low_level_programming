dc5410b0105c1cabe84721680419f07a95b04dec
Bidicte <malexandra13@yahoo.com>
2023-02-26 12:44:46 +0000

    3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,16 +8,16 @@
 
 int main(void)
 {
-  char c = 'a';
-  char ch = 'A';
-  for (c = 'a'; c <= 'z'; c++)
-    {
-      putchar(c);
-    }
-  for (ch = 'A'; ch <= 'Z'; ch++)
-    {
-      putchar(ch);
-    }
-  putchar('\n');
-  return (0);
+char c = 'a';
+char ch = 'A';
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+for (ch = 'A'; ch <= 'Z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
 }
